916db104df0816b26f36a4fa58ea70cd7b875084
Juan Pablo Mayoral <jpmayoral@gmail.com>
2012-08-09 21:50:28 -0300

    agrego nueva interfaz de favoritos

--- assets/www/js/extras.js
+++ assets/www/js/extras.js
@@ -2,7 +2,7 @@ $(document).bind('pageinit', function(){
     $.mobile.defaultPageTransition = 'none';
     $.mobile.defaultDialogTransition = 'none';
 
-   
+   	/* Mostramos la ubicacion por defecto en el centro de San Miguel de Tucuman */
     $('#map_canvas').gmap({'center': '-26.816334, -65.240765','zoom':14});
  
 });
